e08918228f4468101b291c9acad2678b12fcad21
Wettergrund <jonas.wettergrund@gmail.com>
2022-10-15 18:49:10 +0200

    Updated

---
+++ script.js
@@ -0,0 +1,26 @@
+const toptext = document.querySelector(".top")
+const container = document.querySelector(".container")
+const myColors = [  "#443929",
+                    "#b49d37",
+                    "#601e0f",
+                    "#3e5461",
+                    "#a9a8a6",
+                    "#a12523",
+                    "#293e36",
+                    "#ad6a27"
+                        ]
+    
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+let number = getRandomInt(8);
+console.log(number)
+console.log(myColors)
+console.log(myColors[number])
+
+container.style.backgroundColor = myColors[number]
+toptext.style.color = myColors[number]
+
